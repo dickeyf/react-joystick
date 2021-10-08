a6b249e7ff669a128e2d2a5ddb651046e49df097
Francois Dickey <francois.dickey@solacesystems.com>
2021-10-08 17:56:34 -0400

    Implemented stick distance/angle calculations

--- src/joystick.js
+++ src/joystick.js
@@ -1,16 +1,61 @@
 import React from 'react';
 
 export class Joystick extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      moving: false
+    };
+
+    this.handleStart = this.handleStart.bind(this);
+    this.handleMove = this.handleMove.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+  }
+
+
   handleStart(evt) {
-    console.log("Start Event: " + evt);
+    console.log(evt);
+    const startX = evt.clientX;
+    const startY = evt.clientY;
+    console.log("Starting at %d, %d", startX, startY);
+    this.setState({
+        moving: true,
+        startX: startX,
+        startY: startY,
+        distance: 0,
+        angle: 0
+      }
+    );
   }
 
   handleMove(evt) {
-    console.log("Move Event: " + evt);
+    //console.log(evt);
+    if (this.state.moving) {
+      let diffX = evt.clientX - this.state.startX;
+      let diffY = evt.clientY - this.state.startY;
+      let distance = Math.hypot(diffX, diffY);
+      let angle = Math.atan2(diffY, diffX);
+      console.log("Moving: Distance is %d and angle is.", distance, angle);
+
+      this.setState({
+          moving: this.state.moving,
+          startX: this.state.startX,
+          startY: this.state.startY,
+          distance: distance,
+          angle: angle
+        }
+      );
+
+    }
   }
 
   handleEnd(evt) {
-    console.log("End Event: " + evt);
+    //console.log(evt);
+    this.setState({
+        moving: false
+      }
+    );
   }
 
 
@@ -18,12 +63,12 @@ export class Joystick extends React.Component {
     return (
       <div>
         <div
-          className="rounded-full h-36 w-36 flex items-center justify-center bg-gray-600 text-white font-extrabold"
+          className="rounded-full h-36 w-36 flex items-center justify-center bg-gray-600"
           onMouseDown={this.handleStart} onTouchStart={this.handleStart}
           onMouseMove={this.handleMove} onTouchMove={this.handleMove}
           onMouseUp={this.handleEnd} onTouchEnd={this.handleEnd}
         >
-          <div className="rounded-full h-12 w-12 flex items-center justify-center bg-gray-100 text-white font-extrabold"></div>
+          <div className="rounded-full h-12 w-12 bg-gray-100"></div>
         </div>
       </div>
     );
